1da1a4a8ef299fc2ba56eb5087ecc735b10f6c30
niushuang <shuang.niu@funplus.biz>
2022-03-02 04:11:58 +0900

    add laravel ui

--- Laravel/database/migrations/2022_03_01_171228_create_users_table.php
+++ Laravel/database/migrations/2022_03_01_171228_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        if(Schema::hasTable('users'))
+        {
+            Schema::dropIfExists('users');
+        }
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('UserName', 100);
+            $table->string('Password', 20);
+            $table->timestamps();
         });
     }
 
@@ -27,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('users');
     }
 };
