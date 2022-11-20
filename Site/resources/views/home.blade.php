7df8461b7505c06b5e8275762c90081da8451b1b
niushuang <shuang.niu@funplus.biz>
2022-11-20 17:59:39 +0900

    fixed stype

--- Site/resources/views/home.blade.php
+++ Site/resources/views/home.blade.php
@@ -1,16 +1,15 @@
 @extends('base/layout')
-
+<!--Preloader Start-->
+<div class="preloader">
+    <video preload="auto" width="100%" muted autoplay playsinline src="img/top_animation.mp4"></video>
+</div>
+<!--Preloader End-->
 @section('content')
 @section('title', '株式会社最好')
 <!--Modernizr Js-->
 <script src="js/modernizr.js"></script>
 
 <body>
-<!--Preloader Start-->
-<div class="preloader">
-    <video preload="auto" width="100%" muted autoplay playsinline src="img/top_animation.mp4"></video>
-</div>
-<!--Preloader End-->
 
 <!--Ajax Portfolio Container Start-->
 <div class="ajax-portfolio-popup">
@@ -29,7 +28,7 @@
     <ul>
         <li><a href="/carTop"><img src="/img/CarAuction.png"></a></li>
         <li><a href="http://photosite.local/"><img  src="/img/PhotoAuction.png"></a></li>
-        <li><a href="http://businesssupport.local/"><img src="/img/M&AAuction.png"></a></li>
+        <li><a href="http://businesssupport.local/"><img src="/img/HouseAuction.png"></a></li>
     </ul>
 </div>
 
