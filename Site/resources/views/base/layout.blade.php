0292fc85108bd406b8e9b3c2a4c6fa5ded56f472
niushuang <shuang.niu@funplus.biz>
2022-11-20 19:40:14 +0900

    page scale

--- Site/resources/views/base/layout.blade.php
+++ Site/resources/views/base/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
